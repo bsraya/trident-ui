5217cd1a02f62e79b18d0b2851bb2ba6dca40584
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2024-08-24 17:30:33 +0800

    feat: bird logo

---
+++ components/logo.tsx
@@ -0,0 +1,10 @@
+export default function Logo() {
+  return (
+    <div className="bird-logo">
+      <div className="bird-head"></div>
+      <div className="bird-body"></div>
+      <div className="bird-eye"></div>
+      <div className="bird-beak"></div>
+    </div>
+  )
+}
